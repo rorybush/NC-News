6fd20f4c712c37221620bc1d967b0fc08e5854d8
Rory <rorywbush@yahoo.co.uk>
2023-01-13 20:21:48 +0000

    change to how data is displayed for the topic page

--- src/Components/SingleTopic.js
+++ src/Components/SingleTopic.js
@@ -5,6 +5,9 @@ import * as api from "../api";
 import { useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { Link } from "react-router-dom";
+import dayjs from "dayjs";
+const relativeTime = require("dayjs/plugin/relativeTime");
+dayjs.extend(relativeTime);
 
 function SingleTopic() {
   const { topic_slug } = useParams();
@@ -33,7 +36,7 @@ function SingleTopic() {
                     <h3>{article.title}</h3>
                   </Link>
                   <p>By {article.author}</p>
-                  <p>Published: {article.created_at}</p>
+                  <p>Published {dayjs(article.created_at).fromNow()}</p>
                   <p>{article.votes} Votes</p>
                   <p>{article.comment_count} Comments</p>
                 </li>
